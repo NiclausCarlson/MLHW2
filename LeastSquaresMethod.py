124a9b0ac52443422538cdf1668f8ff6a2dfa123
Nik Carlson <gleb_fedorov_00@mail.ru>
2020-11-03 16:03:29 +0300

    Ls done

--- LeastSquaresMethod.py
+++ LeastSquaresMethod.py
@@ -1,6 +1,11 @@
 import Utils as Utils
 import numpy as numpy
 
+
+def computeError(real, predicted):
+    return sum([(zipped[0] - zipped[1]) ** 2 for zipped in zip(predicted, real)])
+
+
 data = Utils.getTrainingData()
 trainingObjects = data[0]
 Utils.normalize(trainingObjects)
@@ -12,34 +17,41 @@ y = [trainingObjects[i][data[1]] for i in range(data[2])]
 for i in range(data[2]):
     del trainingObjects[i][-1]
 
-# добавил единицы как первый столбец ?ЗАЧЕМ?
+# добавил единицы как первый столбец
 ones = numpy.ones((len(trainingObjects), 1))
 trainingObjects = numpy.array(trainingObjects)
 trainingObjects = numpy.hstack((ones, trainingObjects))
 
-singular = numpy.linalg.svd(trainingObjects)
-# возвращает диагональную матрицу как вектор, лол
-print(singular[2])
+singular = numpy.linalg.svd(trainingObjects, full_matrices=False, compute_uv=True)
 
-# не сходятся размерности матриц
 diagSigma = numpy.diag(singular[1])
-invF = singular[2] @ numpy.linalg.pinv(diagSigma) @ singular[0].transpose()
+invF = singular[2].transpose() @ numpy.linalg.pinv(diagSigma) @ singular[0].transpose()
 theta = invF @ y
-# без регуляризации
-print(theta)
+yPredicted = trainingObjects.dot(theta)
+error = computeError(y, yPredicted)
+f = open("leastSquares.txt", "w")
+
+f.write("Without regularization:\n")
+f.write(numpy.array_str(theta) + "\n\n")
+f.write("Error: " + numpy.str(error) + "\n\n")
 
 step = -2
 minError = 100000000000000000000
-idMatrix = numpy.diag(numpy.ones((len(trainingObjects), 1)))
-transposeDiagSigma = numpy.transpose(diagSigma)
+idMatrix = numpy.diag(numpy.ones(len(trainingObjects) + 1))
+minTheta = []
 
-while step != 2:
-    theta = numpy.linalg.inv(transposeDiagSigma @ diagSigma + step * idMatrix) @ transposeDiagSigma @ y
-    yPredicted = trainingObjects @ theta
-    error = sum([(zipped[0] - zipped[1]) ** 2 for zipped in zip(yPredicted, y)])
+while step <= 2:
+    theta = numpy.linalg.inv(
+        trainingObjects.transpose() @ trainingObjects + step * idMatrix) @ trainingObjects.transpose() @ y
+    yPredicted = trainingObjects.dot(theta)
+    error = computeError(y, yPredicted)
     if error < minError:
         minError = error
         minTheta = theta
     step += 0.1
 
-print(minTheta)
+f.write("With regularization:\n")
+f.write(numpy.array_str(minTheta) + "\n\n")
+f.write("Error: " + numpy.str(minError) + "\n")
+
+f.close()
